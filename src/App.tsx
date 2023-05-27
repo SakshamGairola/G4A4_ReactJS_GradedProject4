a262af172f0493dbed1a50abcbe6347f65dfa12a
Saksham <saksham.gairola28@gmail.com>
2023-05-27 19:18:56 +0530

    added `/home` and `/movies-in-theaters` mappings

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ComingSoon from './Pages/ComingSoon';
+import MoviesInTheaters from './Pages/MoviesInTheaters';
+import Home from './Pages/Home';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [movieName, setMovieName] = useState<string>("");
+    const [erro, setError] = useState<Error | null>(null);
+    const [addToFavorite, setAddToFavorite] = useState<number>(0)
+
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home movieName={movieName} />} />
+                <Route path="/home" element={<Home movieName={movieName} />} />
+                {/* <Route path="/coming-soon" element={<ComingSoon movieName={movieName}/>} /> */}
+                <Route path="/movies-in-theaters" element={<MoviesInTheaters movieName={movieName} />} />
+                {/* <Route path="/top-rated-indian" element={<TopRatedIndian movieName={movieName} />} /> */}
+                {/* <Route path="/top-rated-movies" element={<TopRatedMovies movieName={movieName} />} /> */}
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
