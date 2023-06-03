754b2d2153591cb1b72a635c3120a7c4f7ad99df
Saksham <saksham.gairola28@gmail.com>
2023-06-03 18:03:44 +0530

    added link active color

--- src/component/Navbar/Header.tsx
+++ src/component/Navbar/Header.tsx
@@ -28,6 +28,12 @@ function Header({ searchData, moviesList }: { searchData: any, moviesList?: numb
     }, [curentPage]);
 
     if (searchData) searchData(searchText);
+
+    const linkStyle = {
+        onTrue: 'aqua',
+        onFalse: 'black'
+    };
+
     return (
         <Navbar bg="light" expand="lg" fixed="top" onSelect={(eventKey) => { setActiveKey(`${eventKey}`) }}>
             <Container fluid style={{ margin: '0 120px' }}>
@@ -35,12 +41,12 @@ function Header({ searchData, moviesList }: { searchData: any, moviesList?: numb
                 <Navbar.Toggle aria-controls="navbarScroll" />
                 <Navbar.Collapse id="navbarScroll">
                     <Nav defaultActiveKey="home" className="me-auto my-2 my-lg-0" style={{ maxHeight: '100px' }} navbarScroll>
-                        <Nav.Link active={activeKey === 'home'} eventKey={'home'} as={Link} to={'home'}>Home</Nav.Link>
-                        <Nav.Link active={activeKey === 'movies-in-theaters'} eventKey={'movies-in-theaters'} as={Link} to={'movies-in-theaters'}>Movies in Theaters</Nav.Link>
-                        <Nav.Link active={activeKey === 'coming-soon'} eventKey={'coming-soon'} as={Link} to={'coming-soon'} >Coming Soon</Nav.Link>
-                        <Nav.Link active={activeKey === 'top-rated-indian'} eventKey={'top-rated-indian'} as={Link} to={'top-rated-indian'}>Top Rated Indian</Nav.Link>
-                        <Nav.Link active={activeKey === 'top-rated-movies'} eventKey={'top-rated-movies'} as={Link} to={'top-rated-movies'}>Top Rated Movies</Nav.Link>
-                        <Nav.Link active={activeKey === 'favourite'} eventKey={'favourite'} as={Link} to={'favourite'}>Favourite</Nav.Link>
+                        <Nav.Link active={activeKey === 'home'} eventKey={'home'} as={Link} to={'home'} style={{ color: activeKey === 'home' ? linkStyle.onTrue : linkStyle.onFalse }}>Home</Nav.Link>
+                        <Nav.Link active={activeKey === 'movies-in-theaters'} eventKey={'movies-in-theaters'} as={Link} to={'movies-in-theaters'} style={{ color: activeKey === 'movies-in-theaters' ? linkStyle.onTrue : linkStyle.onFalse }} >Movies in Theaters</Nav.Link>
+                        <Nav.Link active={activeKey === 'coming-soon'} eventKey={'coming-soon'} as={Link} to={'coming-soon'} style={{ color: activeKey === 'coming-soon' ? linkStyle.onTrue : linkStyle.onFalse }} >Coming Soon</Nav.Link>
+                        <Nav.Link active={activeKey === 'top-rated-indian'} eventKey={'top-rated-indian'} as={Link} to={'top-rated-indian'} style={{ color: activeKey === 'top-rated-indian' ? linkStyle.onTrue : linkStyle.onFalse }} >Top Rated Indian</Nav.Link>
+                        <Nav.Link active={activeKey === 'top-rated-movies'} eventKey={'top-rated-movies'} as={Link} to={'top-rated-movies'} style={{ color: activeKey === 'top-rated-movies' ? linkStyle.onTrue : linkStyle.onFalse }} >Top Rated Movies</Nav.Link>
+                        <Nav.Link active={activeKey === 'favourite'} eventKey={'favourite'} as={Link} to={'favourite'} style={{ color: activeKey === 'favourite' ? linkStyle.onTrue : linkStyle.onFalse }} >Favourite</Nav.Link>
                     </Nav>
                     <Form className="d-flex">
                         <Form.Control
